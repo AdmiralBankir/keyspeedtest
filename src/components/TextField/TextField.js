d589b392cd1174f061bd0ac65266d2ce77b4b0ac
Владислав Бодин <vladislavbodin@MacBook-Pro-Vladislav.local>
2021-05-17 11:10:19 +0300

    Добавляет получение текста из апи

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TextField = props => {
     return(
         <div>
-            Текстовое поле
+            {props.text}
         </div>
     );
 };
--- src/components/TypeTest/TypeTest.js
+++ src/components/TypeTest/TypeTest.js
@@ -2,13 +2,48 @@ import React from 'react';
 import TextField from '../TextField/TextField';
 import StatField from '../StatField/StatField';
 import Restart from '../Restart/Restart';
+import axios from 'axios';
+
+const fetchSettings = {
+    url: 'https://baconipsum.com/api/',
+    type: 'meat-and-filler',
+    paras: 1,
+    format: 'text'
+};
+
+const getRequestBody = () => {
+    let request = fetchSettings.url + '?';
+    for(let key in fetchSettings) {
+        if(key !== 'url') {
+            request += key + '=' + fetchSettings[key] + '&';
+        }
+    }
+    return request.slice(0, -1);
+};
 
 class TypeTest extends React.Component {
-    state = {};
+    state = {
+        loaded: false,
+        text: ''
+    };
+
+    async componentDidMount() {
+        try {
+            const response  = await axios.get(getRequestBody());
+            this.setState({
+                text: response.data
+            })
+        } catch (e) {
+            alert(e);
+        }
+    }
+
     render() {
         return(
           <main>
-            <TextField />
+            <TextField 
+                text = {this.state.text}
+            />
             <StatField />
             <Restart />
           </main>
